7920b87312f4a764f88df186f067603696133e5a
Jonas Däster <schalk.lpd@gmail.com>
2014-12-18 12:08:37 +0100

    Initialised Project

---
+++ src/ch/fhnw/eai/Main.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.fhnw.eai;
+
+/**
+ *
+ * @author Jonas
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        printKontokorrent();
+        System.out.println("Kontokorrent: "+listeKontokorrentNachname());
+        System.out.println("Sparkontos: "+ listeSparkontoNachname());
+    }
+
+    private static void holeKontoKorrent(java.lang.String queryVorname, java.lang.String queryNachname, javax.xml.ws.Holder<java.lang.String> vorname, javax.xml.ws.Holder<java.lang.String> nachname, javax.xml.ws.Holder<java.lang.String> adresse, javax.xml.ws.Holder<java.lang.String> land, javax.xml.ws.Holder<java.lang.Integer> ranking, javax.xml.ws.Holder<java.lang.String> ibanKontonummer, javax.xml.ws.Holder<java.lang.Float> kontostand, javax.xml.ws.Holder<java.lang.String> bic) {
+        ch.fhnw.wi.eai.bankjd.BankJDService service = new ch.fhnw.wi.eai.bankjd.BankJDService();
+        ch.fhnw.wi.eai.bankjd.BankJD port = service.getBankJDPort();
+        port.holeKontoKorrent(queryVorname, queryNachname, vorname, nachname, adresse, land, ranking, ibanKontonummer, kontostand, bic);
+    }
+
+    private static void holeSparkonto(java.lang.String queryVorname, java.lang.String queryNachname, javax.xml.ws.Holder<java.lang.String> vorname, javax.xml.ws.Holder<java.lang.String> nachname, javax.xml.ws.Holder<java.lang.String> strasse, javax.xml.ws.Holder<java.lang.String> plzOrt, javax.xml.ws.Holder<java.lang.Float> zinsen, javax.xml.ws.Holder<java.lang.Long> kontonummer, javax.xml.ws.Holder<java.lang.Long> kontostand) {
+        ch.fhnw.wi.eai.bankjd.BankJDService service = new ch.fhnw.wi.eai.bankjd.BankJDService();
+        ch.fhnw.wi.eai.bankjd.BankJD port = service.getBankJDPort();
+        port.holeSparkonto(queryVorname, queryNachname, vorname, nachname, strasse, plzOrt, zinsen, kontonummer, kontostand);
+    }
+
+    private static java.util.List<java.lang.String> listeKontokorrentNachname() {
+        ch.fhnw.wi.eai.bankjd.BankJDService service = new ch.fhnw.wi.eai.bankjd.BankJDService();
+        ch.fhnw.wi.eai.bankjd.BankJD port = service.getBankJDPort();
+        return port.listeKontokorrentNachname();
+    }
+
+    private static java.util.List<java.lang.String> listeSparkontoNachname() {
+        ch.fhnw.wi.eai.bankjd.BankJDService service = new ch.fhnw.wi.eai.bankjd.BankJDService();
+        ch.fhnw.wi.eai.bankjd.BankJD port = service.getBankJDPort();
+        return port.listeSparkontoNachname();
+    }
+
+    private static void printKontokorrent() {
+        ch.fhnw.wi.eai.bankjd.BankJDService service = new ch.fhnw.wi.eai.bankjd.BankJDService();
+        ch.fhnw.wi.eai.bankjd.BankJD port = service.getBankJDPort();
+        port.printKontokorrent();
+    }
+
+    private static void printSparkonto() {
+        ch.fhnw.wi.eai.bankjd.BankJDService service = new ch.fhnw.wi.eai.bankjd.BankJDService();
+        ch.fhnw.wi.eai.bankjd.BankJD port = service.getBankJDPort();
+        port.printSparkonto();
+    }
+
+}
