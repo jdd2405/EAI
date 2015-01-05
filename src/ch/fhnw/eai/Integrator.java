0bfce0666ff8586261ffbc988e4425528b90c2fb
Jonas Däster <schalk.lpd@gmail.com>
2015-01-05 12:20:27 +0100

    mapping improvements

--- src/ch/fhnw/eai/Integrator.java
+++ src/ch/fhnw/eai/Integrator.java
@@ -55,9 +55,6 @@ public class Integrator {
         Holder<String> ibanKontonummer = new Holder();
         Holder<Float> kontostand = new Holder();
         Holder<String> bic = new Holder();
-        System.out.println("********************");
-        System.out.println("Kontokorrent BankJD");
-        System.out.println("********************");
             
         ListIterator<String> iterator = ws.listeKontokorrentNachname().listIterator();
         while (iterator.hasNext()){
@@ -65,18 +62,30 @@ public class Integrator {
             queryVorname = null;
             ws.holeKontoKorrent(queryVorname, queryNachname, vorname, nachname, adresse, land, ranking, ibanKontonummer, kontostand, bic);
             
-            System.out.println("Resultat für '"+queryNachname+"' : "+vorname.value+", "+nachname.value+", "+adresse.value+", "+land.value+", "+ranking.value+", "+ibanKontonummer.value+", "+kontostand.value+", "+bic.value);
-            
+                
             Kunde kunde = new Kunde(vorname.value, nachname.value);
             kunde.setAdresse(adresse.value);
             // Quick and Dirty!
             if(kunde.adresse.equals("Kastelgasse 31, 8001 ZH")){
                 kunde.setAdresse("Kastelgasse 31, 8001 Zürich");
-                System.out.println("Geändert zu: "+kunde.adresse);
             }
+            
+            // Ranking Mapping
+            if(ranking.value<3){
+                kunde.setStatus("Gold");
+            }
+            else if(ranking.value==3){
+                kunde.setStatus("Silber");
+            }
+            else {
+                kunde.setStatus("Bronze");
+            }
+            
             Konto konto = new Konto(kunde);
             konto.setKontostand(kontostand.value);
             konto.setIban(ibanKontonummer.value);
+            konto.setKontoart("Kontokorrent");
+            
             
             //Eindeutigkeit prüfen
             eindeutigerKunde = pruefeEindeutigkeit(kunde, kunden);
@@ -119,28 +128,24 @@ public class Integrator {
         Holder<Float> zinsen = new Holder();
         Holder<Long> kontonummer = new Holder();
         Holder<Long> kontostand = new Holder();
-        System.out.println("********************");
-        System.out.println("Sparkonto BankJD");
-        System.out.println("********************");
+
         ListIterator<String> iterator = ws.listeSparkontoNachname().listIterator();
         while (iterator.hasNext()){
             queryNachname = iterator.next();
             queryVorname = null;
             ws.holeSparkonto(queryVorname, queryNachname, vorname, nachname, strasse, plzOrt, zinsen, kontonummer, kontostand);
             
-            System.out.println("Resultat für '"+queryNachname+"' : "+vorname.value+", "+nachname.value+", "+strasse.value+", "+plzOrt.value+", "+zinsen.value+", "+kontonummer.value+", "+kontostand.value);
-            
-            
+           
             Kunde kunde = new Kunde(vorname.value, nachname.value);
             kunde.setAdresse(strasse.value+", "+plzOrt.value);
             // Quick and Dirty!
             if(kunde.adresse.equals("Riggenbachstrasse, , 4600 Olten")){
                 kunde.setAdresse("Riggenbachstrasse, 4600 Olten");
-                System.out.println("Geändert zu: "+kunde.adresse);
             }
             Konto konto = new Konto(kunde);
             konto.setKontostand(kontostand.value);
             konto.setIban(ibanErstellung(kontonummer.value, 0));
+            konto.setKontoart("Sparkonto");
             
             //Eindeutigkeit prüfen
             eindeutigerKunde = pruefeEindeutigkeit(kunde, kunden);
